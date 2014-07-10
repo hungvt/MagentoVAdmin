e0b6100f8f831d919c563a35ef93609f6bc0dab1
hungvt <hungvt.it@gmail.com>
2014-07-10 09:28:34 +0700

    Fix bug
    Fatal error: Call to a member function addItem() on a non-object in
    /app/code/community/VES/VAdmin/Block/Head.php on line 12

--- app/code/community/VES/VAdmin/Block/Head.php
+++ app/code/community/VES/VAdmin/Block/Head.php
@@ -5,16 +5,18 @@ class VES_VAdmin_Block_Head extends Mage_Core_Block_Template
 		parent::_prepareLayout();
 		if(!Mage::getStoreConfig('vadmin/config/enabled')) return;
 		$headBlock = $this->getLayout()->getBlock('head');
-		if(in_array('vendors_default',$this->getLayout()->getUpdate()->getHandles())){
-			$headBlock->removeItem('skin_css','ves_vadmin/css/styles.css');
-			return;
+		if($headBlock){
+			if(in_array('vendors_default',$this->getLayout()->getUpdate()->getHandles())){
+				$headBlock->removeItem('skin_css','ves_vadmin/css/styles.css');
+				return;
+			}
+			$headBlock->addItem('link_rel', Mage::helper('adminhtml')->getUrl("adminhtml/vadmin_css/index"),'rel="stylesheet" type="text/css"');
+			$headBlock->setTitle(Mage::getStoreConfig('vadmin/head/default_title'));
 		}
-		$headBlock->addItem('link_rel', Mage::helper('adminhtml')->getUrl("adminhtml/vadmin_css/index"),'rel="stylesheet" type="text/css"');
-		$headBlock->setTitle(Mage::getStoreConfig('vadmin/head/default_title'));
-		
 		$headerBlock = $this->getLayout()->getBlock('header');
-		$headerBlock->setTemplate('ves_vadmin/page/header.phtml');
-
+		if($headerBlock){
+			$headerBlock->setTemplate('ves_vadmin/page/header.phtml');
+		}
 		/*Disable notification messages*/
 		if(!Mage::getStoreConfig('vadmin/header/show_notifications')){
 			$this->getLayout()->getBlock('root')->unsetChild('notifications');
