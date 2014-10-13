22e07028461f15e92c9b3923f5fee05e91cf01ef
hungvt <hungvt.it@gmail.com>
2014-10-13 15:59:39 +0700

    Fixbug
    Fatal error: Call to a member function unsetChild() on a non-object in
    /app/code/community/VES/VAdmin/Block/Head.php on line 22

--- app/code/community/VES/VAdmin/Block/Head.php
+++ app/code/community/VES/VAdmin/Block/Head.php
@@ -5,6 +5,7 @@ class VES_VAdmin_Block_Head extends Mage_Core_Block_Template
 		parent::_prepareLayout();
 		if(!Mage::getStoreConfig('vadmin/config/enabled')) return;
 		$headBlock = $this->getLayout()->getBlock('head');
+		
 		if($headBlock){
 			if(in_array('vendors_default',$this->getLayout()->getUpdate()->getHandles())){
 				$headBlock->removeItem('skin_css','ves_vadmin/css/styles.css');
@@ -13,12 +14,15 @@ class VES_VAdmin_Block_Head extends Mage_Core_Block_Template
 			$headBlock->addItem('link_rel', Mage::helper('adminhtml')->getUrl("adminhtml/vadmin_css/index"),'rel="stylesheet" type="text/css"');
 			$headBlock->setTitle(Mage::getStoreConfig('vadmin/head/default_title'));
 		}
+		
 		$headerBlock = $this->getLayout()->getBlock('header');
 		if($headerBlock){
 			$headerBlock->setTemplate('ves_vadmin/page/header.phtml');
 		}
+		
 		/*Disable notification messages*/
-		if(!Mage::getStoreConfig('vadmin/header/show_notifications')){
+		$notificationBlock = $this->getLayout()->getBlock('notifications');
+		if(!Mage::getStoreConfig('vadmin/header/show_notifications') && $notificationBlock){
 			$this->getLayout()->getBlock('root')->unsetChild('notifications');
 		}
 		
